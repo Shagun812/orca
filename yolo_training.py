510bfcd5504b17fd52b3036e509cd35422bdcfd1
Aarav Jain <aaravjain366@gmail.com>
2026-09-08 13:03:43 +0530

    Added Training Pipeline

--- yolo_training.py
+++ yolo_training.py
@@ -1,18 +1,22 @@
 from ultralytics import YOLO
 
-# Load pretrained YOLO model
-model = YOLO("yolo11n.pt")
+def main():
+    # Load pretrained YOLO model
+    model = YOLO("yolo11n.pt")
 
-# Train
-results = model.train(
-    data="DARTIS_YOLO/data.yaml",
-    epochs=30,
-    imgsz=640,
-    batch=8,
-    project="runs/orca",
-    name="oil_detector"
-)
+    # Train
+    results = model.train(
+        data="DARTIS_YOLO/data.yaml",
+        epochs=30,
+        imgsz=640,
+        batch=8,
+        project="runs/orca",
+        name="orca"
+    )
 
-print("Training completed!")
-print("Best model saved at:")
-print("runs/orca/oil_detector/weights/best.pt")
+    print("Training completed!")
+    print("Best model saved at:")
+    print("runs/orca/oil_detector/weights/best.pt")
+
+if __name__ == "__main__":
+    main()
